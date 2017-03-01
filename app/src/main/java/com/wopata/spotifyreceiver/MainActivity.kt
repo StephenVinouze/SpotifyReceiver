ff01948a092c19970ed3fe38990eda3ed4a05d43
StephenVinouze <s.vinouze@gmail.com>
2017-03-01 10:56:36 +0100

    Redirect to track

--- app/src/main/java/com/wopata/spotifyreceiver/MainActivity.kt
+++ app/src/main/java/com/wopata/spotifyreceiver/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.wopata.spotifyreceiver
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
 import com.wopata.spotifyreceiver.models.Track
 import com.wopata.spotifyreceiver.receivers.SpotifyBroadcastReceiver
 import com.wopata.spotifyreceiver.views.PlayerBannerView
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity(), SpotifyBroadcastReceiver.OnSpotifyEven
     @BindView(R.id.player_banner)
     lateinit var playerBanner: PlayerBannerView
 
+    private var track: Track? = null
     private val spotifyBroadcastReceiver = SpotifyBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +37,16 @@ class MainActivity : AppCompatActivity(), SpotifyBroadcastReceiver.OnSpotifyEven
         super.onPause()
     }
 
+    @OnClick(R.id.player_banner)
+    fun onRedirectToTrackClick() {
+        val trackId = track?.trackId?.split(":")?.get(2)
+        if (trackId != null) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://open.spotify.com/track/$trackId")))
+        }
+    }
+
     override fun onMetadataChanged(track: Track) {
+        this.track = track
         playerBanner.bind(track)
     }
 
